44ffa6f2c5de0a73d732ef3d2b9411664363cedf
Jakub <meowmme@gmail.com>
2023-10-16 23:58:15 +0200

    Initial commit

---
+++ src/Func.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Func {
+    static public void menu(){
+        System.out.println("=========Tic Tac Toe=========");
+        System.out.println("||   Please select option  ||");
+        System.out.println("||         1. Play         ||");
+        System.out.println("||         2. Info         ||");
+        System.out.println("||         3. Exit         ||");
+        System.out.println("=============================");
+    }
+
+    static public void gamemode(){
+        System.out.println("=========Tic Tac Toe=========");
+        System.out.println("|| Please select game mode ||");
+        System.out.println("||         1. Solo         ||");
+        System.out.println("||         2. Duo          ||");
+        System.out.println("||         3. Back         ||");
+        System.out.println("=============================");
+    }
+
+    static public void info(){
+        System.out.println("==========Game Info==========");
+        System.out.println("||   The aim of the game   ||");
+        System.out.println("||   is to arrange three   ||");
+        System.out.println("||  characters in one row  ||");
+        System.out.println("|| vertically, horizontally||");
+        System.out.println("||       or diagonally     ||");
+        System.out.println("|| The first player to put ||");
+        System.out.println("|| the symbols in one line ||");
+        System.out.println("||      wins the game!     ||");
+        System.out.println("=============================");
+    }
+
+    static public void draw(Object[] board){
+        Game.displayBoard(board);
+        System.out.println("============System===========");
+        System.out.println("||       It's a draw!      ||");
+        System.out.println("=============================");
+    }
+
+    static public void win(Object[] board, char player){
+        Game.displayBoard(board);
+        System.out.println("============System===========");
+        System.out.println("||      Player "+ player +" wins!     ||");
+        System.out.println("=============================");
+    }
+
+    static public void sysFarewell(){
+        System.out.println("============System===========");
+        System.out.println("||     See Ya next time!   ||");
+        System.out.println("=============================");
+    }
+
+    static public void sysError(){
+        System.out.println("============System===========");
+        System.out.println("||   Oops! Wrong option!   ||");
+        System.out.println("=============================");
+    }
+
+    static public int scanInt(){
+        int x;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(">> ");
+        x = scanner.nextInt();
+        return x;
+    }
+}
---
+++ src/Game.java
@@ -0,0 +1,151 @@
+public class Game {
+    static public Object[] cleanBoard() {
+        Object[] board = new Object[9];
+        int i = 0;
+        while (i < 9) {
+            board[i] = i + 1;
+            i++;
+        }
+        return board;
+    }
+
+    static public void displayBoard(Object[] board) {
+        System.out.println("============Board============");
+        for (int i = 0; i < 9; i++) {
+            if (i == 0) {
+                System.out.print("||          ");
+            } else if (i == 3 || i == 6) {
+                System.out.print("\n||          ");
+
+            }
+            System.out.print(board[i] + " ");
+            if(i == 2 || i == 5 || i == 8){
+                System.out.print("          ||");
+            }
+        }
+        System.out.println("\n=============================");
+    }
+
+    static public void playerPick(Object[] board, char player) {
+        Game.displayBoard(board);
+        System.out.println("Player "+player+" place your choice: ");
+        int playerChoice = Func.scanInt();
+        if (board[playerChoice - 1] instanceof Number) {
+            board[playerChoice - 1] = player;
+        } else {
+            System.out.println("Incorrect pick!");
+            playerPick(board, player);
+        }
+    }
+
+    static public boolean checkIfDraw(Object[] board) {
+        boolean game = true;
+        int filledNodes = 0;
+        for (int i = 0; i < 9; i++) {
+            if (board[i] instanceof Character) {
+                filledNodes++;
+            }
+        }
+        if (filledNodes >= 9) {
+            game = false;
+        }
+        return game;
+    }
+
+    public static int checkWinCondition(Object[] board, char player, char player1) {
+        char t1, t2, t3;
+        int winner = 0;
+        for (int i = 0; i < 8; i += 3) {
+            if (board[i] instanceof Character && board[i + 1] instanceof Character && board[i + 2] instanceof Character) {
+                t1 = (char) board[i];
+                t2 = (char) board[i + 1];
+                t3 = (char) board[i + 2];
+                if (t1 == player && t1 == t2 && t2 == t3) {
+                    if(t1 == player1){
+                        winner = 1;
+                    }else{
+                        winner = 2;
+                    }
+                    return winner;
+                }
+            }
+        }
+
+        for (int i = 0; i < 3; i += 1) {
+            if (board[i] instanceof Character && board[i + 3] instanceof Character && board[i + 6] instanceof Character) {
+                t1 = (char) board[i];
+                t2 = (char) board[i + 3];
+                t3 = (char) board[i + 6];
+                if (t1 == player && t1 == t2 && t2 == t3) {
+                    if(t1 == player1){
+                        winner = 1;
+                    }else{
+                        winner = 2;
+                    }
+                    return winner;
+                }
+            }
+        }
+        if (board[0] instanceof Character && board[4] instanceof Character && board[8] instanceof Character) {
+            t1 = (char) board[0];
+            t2 = (char) board[4];
+            t3 = (char) board[8];
+            if (t1 == player && t1 == t2 && t2 == t3) {
+                if(t1 == player1){
+                    winner = 1;
+                }else{
+                    winner = 2;
+                }
+                return winner;
+            }
+        }
+        if (board[2] instanceof Character && board[4] instanceof Character && board[6] instanceof Character) {
+            t1 = (char) board[2];
+            t2 = (char) board[4];
+            t3 = (char) board[6];
+            if (t1 == player && t1 == t2 && t2 == t3) {
+                if(t1 == player1){
+                    winner = 1;
+                }else{
+                    winner = 2;
+                }
+                return winner;
+            }
+        }
+
+        return winner;
+    }
+
+    static public void duo(boolean game, char player1, char player2){
+        int winner;
+        Object[] board;
+        board = Game.cleanBoard();
+        while (game) {
+            Game.playerPick(board, player1);
+            winner = Game.checkWinCondition(board, player1, player1);
+            if (winner == 1) {
+                Func.win(board, player1);
+                break;
+            }
+
+            game = Game.checkIfDraw(board);
+            if (!game) {
+                Func.draw(board);
+                break;
+            }
+
+            Game.playerPick(board, player2);
+            winner = Game.checkWinCondition(board, player2, player1);
+            if (winner == 2) {
+                Func.win(board, player2);
+                break;
+            }
+
+            game = Game.checkIfDraw(board);
+            if (!game) {
+                Func.draw(board);
+                break;
+            }
+        }
+    }
+}
